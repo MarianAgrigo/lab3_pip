694417fc6d7e780534262372da8d004b8588a88c
Agrigoroaie Marian <100911701+MarianAgrigo@users.noreply.github.com>
2022-03-11 09:12:56 +0200

    am adaugat o intrebare

--- src/lab3_pip_pr/main.java
+++ src/lab3_pip_pr/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("salut salut");
+		System.out.println("cum mai esti?");
+		
 
 	}
 
