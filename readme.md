f1ff10d91757f2eeca59394f2750322c60a3f2b3
Agrigoroaie Marian <100911701+MarianAgrigo@users.noreply.github.com>
2022-03-11 09:01:02 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+#Primul proiect 
+##demosntratie
+
+Agrigoroaie Marian
+marian.agrigoroaie@student.tuiasi.ro	
+
+site:
+https://github.com/MarianAgrigo/lab3_pip.git
+
---
+++ src/lab3_pip_pr/main.java
@@ -0,0 +1,11 @@
+package lab3_pip_pr;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("salut salut");
+
+	}
+
+}
